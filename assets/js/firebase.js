a0c345e2abbd1b7b1da0e064b3baccfc0a272270
taus-if <65533241+taus-if@users.noreply.github.com>
2022-01-14 13:20:03 +0600

    final 1
    final 1 done

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,20 +1,25 @@
 function submitContact() {
-	var name = document.getElementById("name").value;
-	var email = document.getElementById("email").value;
-	var subject = document.getElementById("subject").value;
-	var message = document.getElementById("message").value;
-	var result = email.split(".", 1);
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var subject = document.getElementById("subject").value;
+  var message = document.getElementById("message").value;
+  var result = email.split(".", 1);
 
-     firebase.database().ref('Contact/' + name).set({
-            name : name,
-			subject : subject,
-            email : email,
-			message : message
-          }, function(error) {
-            if (error) {
-              // The write failed...
-            } else {
-                alert("Message sent !");
-            }
-          });
- }
+  firebase.database().ref('Contact/' + name).set({
+    name: name,
+    subject: subject,
+    email: email,
+    message: message
+  }, function (error) {
+    if (error) {
+      // The write failed...
+    } else {
+      location.reload();
+      document.getElementById("name").value="";
+      document.getElementById("email").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("message").value = "";
+      alert("Message sent !");
+    }
+  });
+}
